25315e5368df0235338a660f53a03b10643c341f
Stefanos Amanuel <stefano.amanuel@me.com>
2022-05-14 19:57:12 -0400

    testing with translate

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 import pytesseract
+import translaters as ts 
 # import cv2
 # import PIL.Image
 
 
 my_config = r'--oem 3 --psm 6'
 
-text = pytesseract.image_to_string(PIL.Image.open('./images/ECL8R.PNG'), config=my_config)
+img_text = pytesseract.image_to_string(PIL.Image.open('./images/ECL8R.PNG'), config=my_config)
 
-print(text)
+print(ts.google(img_text, to_language='es'))
