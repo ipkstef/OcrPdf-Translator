f8dfe0e0542ee28ef1e6034658676a95c29a7e75
Stefanos Amanuel <stefano.amanuel@me.com>
2022-05-15 01:26:48 -0400

    okokokokoko

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ images = convert_from_bytes(open('/app/images/excerpts.pdf', 'rb').read())
 
 for page in images:
     count = 0
-    page.PIL.Image.save(f'/app/images/multitest{count}.png', 'PNG')
-    image = cv2.imread(f'/app/images/multitest{count}.png')
+    PIL.Image.save(f'/app/images/{page}{count}.png', 'PNG')
+    image = cv2.imread(f'/app/images/{page}{count}.png')
     text = pytesseract.image_to_string(image, config=my_config)
     print(text)
     print('\n\n')
