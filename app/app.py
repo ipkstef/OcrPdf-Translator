b7f614ff711fe213af33d2c5953cf9480da3a2d7
Stefanos Amanuel <stefano.amanuel@me.com>
2022-05-14 20:50:20 -0400

    asd

--- app/app.py
+++ app/app.py
@@ -6,6 +6,11 @@ import PIL.Image
 
 my_config = r'--oem 3 --psm 6'
 
-img_text = pytesseract.image_to_string(PIL.Image.open('./images/engtest.png'), config=my_config)
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-print(ts.google(img_text, to_language='es'))
+print(dir_path)
+
+# img_text = pytesseract.image_to_string(PIL.Image.open('/app/images/engtest.png'), config=my_config)
+
+# print(ts.google(img_text, to_language='es'))
