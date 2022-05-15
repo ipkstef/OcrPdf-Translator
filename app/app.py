1f75306fc4ee1dca5e2d868b3c0a2da12c5def35
Stefanos Amanuel <stefano.amanuel@me.com>
2022-05-15 01:04:18 -0400

    quick test

--- app/app.py
+++ app/app.py
@@ -16,8 +16,15 @@ my_config = r'--oem 3 --psm 6'
 
 images = convert_from_bytes(open('/app/images/recipe.pdf', 'rb').read())
 
-img_text = pytesseract.image_to_string(PIL.Image.open(images), config=my_config)
+for page in images:
+    page.save('/app/images/page.png', 'PNG')
+    image = cv2.imread('/app/images/page.png')
+    text = pytesseract.image_to_string(image, config=my_config)
+    print(text)
+    print('\n\n')
+
+# img_text = pytesseract.image_to_string(PIL.Image.open(images), config=my_config)
 
 
-print(img_text)
+# print(img_text)
 # print(ts.google(img_text, to_language='en'))
