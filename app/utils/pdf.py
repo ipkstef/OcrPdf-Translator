0e9e1989702aedafc3d816b08bae4a7d90a3afd0
Stefanos Amanuel <stefano.amanuel@me.com>
2022-05-18 00:14:17 -0400

    huge working refactor

--- app/utils/pdf.py
+++ app/utils/pdf.py
@@ -1,17 +1,3 @@
-from pdf2image import convert_from_path, convert_from_bytes
-import pytesseract
-import PIL.Image
-
-
-
-from pdf2image.exceptions import (
-    PDFInfoNotInstalledError,
-    PDFPageCountError,
-    PDFSyntaxError
-)
-
-import os
-
 class InputPdf():
     def __init__(self):
         self.pdf = None
@@ -21,15 +7,8 @@ class InputPdf():
         self.pdf = pdf_file
         
     
-    def image_paths(self, path):
-        image_paths = []
-        for filename in os.listdir(path):
-            if filename.endswith(".png"):
-                image_paths.append(path + filename)
-        for image_path in image_paths:
-            with open(path + 'images.txt', 'a') as file:
-                file.write(image_path + '\n')
-        return image_paths
+    def 
+
 
 
 
